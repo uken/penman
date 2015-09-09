743038796798dd7928aba03d9befc6f3a7375348
mat <matpataki@gmail.com>
2015-09-09 18:38:39 -0400

    add migration

---
+++ db/migrate/20150909222413_create_record_tags.rb
@@ -0,0 +1,13 @@
+class CreateRecordTags < ActiveRecord::Migration
+  def change
+    create_table :record_tags do |t|
+      t.integer :record_id, null: false, default: 0
+      t.string :record_type, null: false
+      t.string :tag, null: false
+      t.boolean :created_this_session, null: false, default: false
+      t.timestamps
+    end
+
+    add_index 'record_tags', ['record_id', 'record_type'], name: 'index_record_tags_on_record_id_and_record_type', using: :btree
+  end
+end
