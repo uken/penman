06088059bca0723658bc4c4cac40fb994ae1aae3
mat <matpataki@gmail.com>
2015-09-28 15:36:41 -0400

    make some RecordTag method private

--- lib/record_tags/record_tag.rb
+++ lib/record_tags/record_tag.rb
@@ -30,131 +30,131 @@ class RecordTag < ActiveRecord::Base
   @@tree        = {}
   @@polymorphic = []
 
-  def self.disable
-    @@enabled = false
-  end
-
-  def self.enable
-    @@enabled = true
-  end
-
-  def self.register(model)
-    reflections = model.reflect_on_all_associations(:belongs_to)
-
-    if reflections.find { |r| r.options[:polymorphic] }.present?
-      @@polymorphic << model
-    else
-      @@roots.push(model) unless @@tree.key?(model)
-    end
-
-    @@tree[model] = reflections.reject { |r| r.options[:polymorphic] || r.klass == model }.map(&:klass)
-    @@tree[model].each { |ch| @@tree[ch] ||= [] }
-
-    @@roots -= @@tree[model]
-  end
-
-  def self.tag(record, tag)
-    return unless @@enabled
-    candidate_key = record.class.try(:candidate_key) || :reference
-    candidate_key = [candidate_key] unless candidate_key.is_a? Array
-    raise RecordTagExceptions::InvalidCandidateKeyForRecord unless self.record_has_attributes?(record, candidate_key)
-
-    candidate_key_to_store =
-      if ['created', 'destroyed'].include? tag
-        Hash[candidate_key.map { |k| [k, record.send(k)] }].to_json
-      else # updated
-        Hash[candidate_key.map { |k| [k, record.send("#{k}_was")] }].to_json
-      end
-
-    created_tag = RecordTag.find_by(record: record, tag: 'created')
-    updated_tag = RecordTag.find_by(record: record, tag: 'updated')
-    destroyed_tag = RecordTag.find_by(record_type: record.class.name, candidate_key: candidate_key_to_store, tag: 'destroyed')
-
-    raise RecordTagExceptions::TooManyTagsForRecord if [created_tag, updated_tag, destroyed_tag].count { |t| t.present? } > 1
-
-    if created_tag.present?
-      case tag
-      when 'created'
-        raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('created', 'created', candidate_key_to_store)
-      when 'updated'
-        created_tag.update!(tag: tag)
-      when 'destroyed'
-        created_tag.destroy!
-      end
-    elsif updated_tag.present?
-      case tag
-      when 'created'
-        raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('updated', 'created', candidate_key_to_store)
-      when 'updated'
-        updated_tag.update!(tag: tag)
-      when 'destroyed'
-        if updated_tag.created_this_session
-          updated_tag.destroy!
-        else
-          updated_tag.update!(tag: tag)
-        end
-      end
-    elsif destroyed_tag.present?
-      case tag
-      when 'created'
-        # We make an updated tag in case non-candidate key attributes have changed, since we don't tack those.
-        destroyed_tag.update!(tag: 'updated', record_id: record.id)
-      when 'updated'
-        raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('destroyed', 'updated', candidate_key_to_store)
-      when 'destroyed'
-        raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('destroyed', 'destroyed', candidate_key_to_store)
-      end
-    else # new tag
-      RecordTag.create!(record: record, tag: tag, candidate_key: candidate_key_to_store, created_this_session: tag == 'created')
-    end
-  end
-
-  def self.find_tags_for_model(model)
-    self.find_tags_for_models(model)
-  end
-
-  def self.find_tags_for_models(*models)
-    RecordTag.where(record_type: models.map { |m| (m.is_a? String) ? m : m.class_name })
-  end
-
-  def self.create_custom(attributes = {})
-    attributes = { record_type: 'custom_tag', tag: 'touched', candidate_key: 'n/a' }.merge attributes
-    record_tag = RecordTag.find_or_create_by(attributes)
-    record_tag.update(record_id: record_tag.id) if record_tag.record_id == 0 # notice validation above, this just ensures that we don't violate the table constraint.
-  end
-
-  def self.generate_seeds
-    self.generate_seed_for_models(self.seed_order)
-  end
-
-  def self.generate_seed_for_models(models)
-    time = Time.now
-    seed_files = []
-
-    models.each do |model|
-      seed_files << self.generate_update_seed(model, time.strftime('%Y%m%d%H%M%S'))
-      time += 1.second
-    end
-
-    models.reverse.each do |model|
-      seed_files << self.generate_destroy_seed(model, time.strftime('%Y%m%d%H%M%S'))
-      time += 1.second
-    end
-
-    RecordTag.where(record_type: models.map(&:name)).destroy_all
-    seed_files.compact
-  end
-
-  def self.generate_seed_for_model(model)
-    time = Time.now
-    seed_files = []
-    seed_files << self.generate_update_seed(model, time.strftime('%Y%m%d%H%M%S'))
-    seed_files << self.generate_destroy_seed(model, (time + 1.second).strftime('%Y%m%d%H%M%S'))
-    RecordTag.where(record_type: model.name).destroy_all
-    seed_files.compact
-  end
-
   class << self
+    def disable
+      @@enabled = false
+    end
+
+    def enable
+      @@enabled = true
+    end
+
+    def register(model)
+      reflections = model.reflect_on_all_associations(:belongs_to)
+
+      if reflections.find { |r| r.options[:polymorphic] }.present?
+        @@polymorphic << model
+      else
+        @@roots.push(model) unless @@tree.key?(model)
+      end
+
+      @@tree[model] = reflections.reject { |r| r.options[:polymorphic] || r.klass == model }.map(&:klass)
+      @@tree[model].each { |ch| @@tree[ch] ||= [] }
+
+      @@roots -= @@tree[model]
+    end
+
+    def tag(record, tag)
+      return unless @@enabled
+      candidate_key = record.class.try(:candidate_key) || :reference
+      candidate_key = [candidate_key] unless candidate_key.is_a? Array
+      raise RecordTagExceptions::InvalidCandidateKeyForRecord unless record_has_attributes?(record, candidate_key)
+
+      candidate_key_to_store =
+        if ['created', 'destroyed'].include? tag
+          Hash[candidate_key.map { |k| [k, record.send(k)] }].to_json
+        else # updated
+          Hash[candidate_key.map { |k| [k, record.send("#{k}_was")] }].to_json
+        end
+
+      created_tag = RecordTag.find_by(record: record, tag: 'created')
+      updated_tag = RecordTag.find_by(record: record, tag: 'updated')
+      destroyed_tag = RecordTag.find_by(record_type: record.class.name, candidate_key: candidate_key_to_store, tag: 'destroyed')
+
+      raise RecordTagExceptions::TooManyTagsForRecord if [created_tag, updated_tag, destroyed_tag].count { |t| t.present? } > 1
+
+      if created_tag.present?
+        case tag
+        when 'created'
+          raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('created', 'created', candidate_key_to_store)
+        when 'updated'
+          created_tag.update!(tag: tag)
+        when 'destroyed'
+          created_tag.destroy!
+        end
+      elsif updated_tag.present?
+        case tag
+        when 'created'
+          raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('updated', 'created', candidate_key_to_store)
+        when 'updated'
+          updated_tag.update!(tag: tag)
+        when 'destroyed'
+          if updated_tag.created_this_session
+            updated_tag.destroy!
+          else
+            updated_tag.update!(tag: tag)
+          end
+        end
+      elsif destroyed_tag.present?
+        case tag
+        when 'created'
+          # We make an updated tag in case non-candidate key attributes have changed, since we don't tack those.
+          destroyed_tag.update!(tag: 'updated', record_id: record.id)
+        when 'updated'
+          raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('destroyed', 'updated', candidate_key_to_store)
+        when 'destroyed'
+          raise RecordTagExceptions::BadTracking, RecordTag.format_error_message('destroyed', 'destroyed', candidate_key_to_store)
+        end
+      else # new tag
+        RecordTag.create!(record: record, tag: tag, candidate_key: candidate_key_to_store, created_this_session: tag == 'created')
+      end
+    end
+
+    def find_tags_for_model(model)
+      find_tags_for_models(model)
+    end
+
+    def find_tags_for_models(*models)
+      RecordTag.where(record_type: models.map { |m| (m.is_a? String) ? m : m.name })
+    end
+
+    def create_custom(attributes = {})
+      attributes = { record_type: 'custom_tag', tag: 'touched', candidate_key: 'n/a' }.merge attributes
+      record_tag = RecordTag.find_or_create_by(attributes)
+      record_tag.update(record_id: record_tag.id) if record_tag.record_id == 0 # notice validation above, this just ensures that we don't violate the table constraint.
+    end
+
+    def generate_seeds
+      generate_seed_for_models(seed_order)
+    end
+
+    def generate_seed_for_models(models)
+      time = Time.now
+      seed_files = []
+
+      models.each do |model|
+        seed_files << generate_update_seed(model, time.strftime('%Y%m%d%H%M%S'))
+        time += 1.second
+      end
+
+      models.reverse.each do |model|
+        seed_files << generate_destroy_seed(model, time.strftime('%Y%m%d%H%M%S'))
+        time += 1.second
+      end
+
+      RecordTag.where(record_type: models.map(&:name)).destroy_all
+      seed_files.compact
+    end
+
+    def generate_seed_for_model(model)
+      time = Time.now
+      seed_files = []
+      seed_files << generate_update_seed(model, time.strftime('%Y%m%d%H%M%S'))
+      seed_files << generate_destroy_seed(model, (time + 1.second).strftime('%Y%m%d%H%M%S'))
+      RecordTag.where(record_type: model.name).destroy_all
+      seed_files.compact
+    end
+
     private
 
     def seed_order
@@ -176,11 +176,11 @@ class RecordTag < ActiveRecord::Base
       touched_tags = RecordTag.where(record_type: model.name, tag: ['created', 'updated']).includes(:record)
       return nil if touched_tags.empty?
 
-      base_file_path = File.join(Rails.root, 'db', 'packs') #TODO throw this into a config
-      seed_path = File.join(base_file_path, "#{timestamp}_seed_#{model.class_name.underscore.pluralize}_updates.rb")
+      base_file_path = File.join(Rails.root, 'db', 'migrate') #TODO throw this into a config
+      seed_path = File.join(base_file_path, "#{timestamp}_seed_#{model.name.underscore.pluralize}_updates.rb")
 
       File.open(seed_path, 'w') do |f|
-        f.puts "class Seed#{model.class_name.pluralize}Updates"
+        f.puts "class Seed#{model.name.pluralize}Updates"
         f.puts '  def up'
         f.puts '    # Generated by RecordTag.generate_seed'
 
@@ -188,18 +188,18 @@ class RecordTag < ActiveRecord::Base
           f.puts "\n"
           f.puts "    # Generating seed for #{tag.tag.upcase} tag."
           f.print  "    record = #{model.name}.find_by("
-          f.print self.print_candidate_key(tag.record)
+          f.print print_candidate_key(tag.record)
           f.puts  ')'
 
           f.print  "    record = #{model.name}.find_or_initialize_by("
-          f.print self.attribute_string_from_hash(model, tag.candidate_key)
+          f.print attribute_string_from_hash(model, tag.candidate_key)
           f.puts ') if record.nil?'
 
           f.print "    record.update!("
           column_hash = {}
           model.attribute_names.reject { |col| col == model.primary_key }
                                .each { |col| column_hash[col] = tag.record.send(col) }
-          f.print self.attribute_string_from_hash(model, column_hash)
+          f.print attribute_string_from_hash(model, column_hash)
           f.puts ')'
         end
 
@@ -214,18 +214,18 @@ class RecordTag < ActiveRecord::Base
       destroyed_tags = RecordTag.where(record_type: model.name, tag: 'destroyed')
       return nil if destroyed_tags.empty?
 
-      base_file_path = File.join(Rails.root, 'db', 'packs') #TODO throw this into a config
-      seed_path = File.join(base_file_path, "#{timestamp}_seed_#{model.class_name.underscore.pluralize}_destroys.rb")
+      base_file_path = File.join(Rails.root, 'db', 'migrate') #TODO throw this into a config
+      seed_path = File.join(base_file_path, "#{timestamp}_seed_#{model.name.underscore.pluralize}_destroys.rb")
 
       File.open(seed_path, 'w') do |f|
-        f.puts "class Seed#{model.class_name.pluralize}Destroys"
+        f.puts "class Seed#{model.name.pluralize}Destroys"
         f.puts '  def up'
         f.puts '    # Generated by RecordTag.generate_seed'
 
         destroyed_tags.map(&:candidate_key).each do |record_candidate_key|
           f.puts "\n"
           f.print "    record = #{model.name}.find_by("
-          f.puts "#{self.attribute_string_from_hash(model, record_candidate_key)})"
+          f.puts "#{attribute_string_from_hash(model, record_candidate_key)})"
           f.puts "    record.try(:destroy)"
         end
 
@@ -239,11 +239,11 @@ class RecordTag < ActiveRecord::Base
     def print_candidate_key(record)
       candidate_key = record.class.try(:candidate_key) || :reference
       candidate_key = [candidate_key] unless candidate_key.is_a? Array
-      raise RecordTagExceptions::InvalidCandidateKeyForRecord unless self.record_has_attributes?(record, candidate_key)
+      raise RecordTagExceptions::InvalidCandidateKeyForRecord unless record_has_attributes?(record, candidate_key)
 
       candidate_key_hash = {}
       candidate_key.each { |key| candidate_key_hash[key] = record.send(key) }
-      self.attribute_string_from_hash(record.class, candidate_key_hash)
+      attribute_string_from_hash(record.class, candidate_key_hash)
     end
 
     def attribute_string_from_hash(model, column_hash)
@@ -251,7 +251,7 @@ class RecordTag < ActiveRecord::Base
       formatted_candidate_key = []
 
       column_hash.each do |k, v|
-        reflection = self.find_foreign_key_relation(model, k)
+        reflection = find_foreign_key_relation(model, k)
 
         if reflection && v.present?
           associated_model = if reflection.polymorphic?
@@ -265,11 +265,11 @@ class RecordTag < ActiveRecord::Base
             to_add = "#{reflection.name}: #{associated_model.name}.find_by("
 
             if associated_record.present?
-              to_add += "#{self.print_candidate_key(associated_record)})"
+              to_add += "#{print_candidate_key(associated_record)})"
             else # likely this record was destroyed, so we should have a tag for it
               tag = RecordTag.find_by(record_type: associated_model.name, record_id: v)
               raise RecordTagExceptions::RecordNotFound, "while processing #{column_hash}" if tag.nil?
-              to_add += "#{self.attribute_string_from_hash(associated_model, tag.candidate_key)})"
+              to_add += "#{attribute_string_from_hash(associated_model, tag.candidate_key)})"
             end
 
             formatted_candidate_key << to_add
@@ -277,7 +277,7 @@ class RecordTag < ActiveRecord::Base
           end
         end
 
-        formatted_candidate_key << "#{k}: #{self.primitive_string(v)}"
+        formatted_candidate_key << "#{k}: #{primitive_string(v)}"
       end
 
       formatted_candidate_key.join(', ')
