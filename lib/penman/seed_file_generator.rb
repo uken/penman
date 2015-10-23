8513ce9e3d29ebeabe48fe80cb44b0b1def92e4f
mat <matpataki@gmail.com>
2015-10-22 22:40:32 -0400

    setup default seed template erb

--- lib/penman/seed_file_generator.rb
+++ lib/penman/seed_file_generator.rb
@@ -1,16 +1,21 @@
 module Penman
   class SeedFileGenerator
     attr_reader :seed_code
+    attr_reader :file_name
     attr_reader :timestamp
 
-    def initialize(seed_code, timestamp)
+    def initialize(file_name, timestamp, seed_code)
       @seed_code = seed_code
+      @file_name = file_name
       @timestamp = timestamp
     end
 
     def write_seed
-      erb = ERB.new(File.read('guy_template.erb')) # TODO change this to a config setting
-      result = erb.result(binding)
+      erb = ERB.new(File.read(Penman.config.seed_template_file))
+      seed_file_name = "#{@timestamp}_#{@file_name}.rb" # TODO config a file name formatter
+      full_seed_file_path = File.join(Penman.config.seed_path, seed_file_name)
+      IO.write(full_seed_file_path, erb.result(binding))
+      full_seed_file_path
     end
   end
 end
