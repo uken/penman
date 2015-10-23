e097f171bb181af95780c424abe2c1ce430e5289
mat <matpataki@gmail.com>
2015-10-22 21:33:29 -0400

    add config for template file

--- lib/penman/configuration.rb
+++ lib/penman/configuration.rb
@@ -3,11 +3,13 @@ module Penman
     attr_accessor :seed_path
     attr_accessor :default_candidate_key
     attr_accessor :seed_method_name
+    attr_accessor :seed_template_file
 
     def initialize
       @seed_path = 'db/migrate'
       @default_candidate_key = :reference
       @seed_method_name = :change
+      @seed_template_file = nil
     end
   end
 end
