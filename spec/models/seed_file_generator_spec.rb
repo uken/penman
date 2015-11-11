b4f66ca2231891a9984e6d1c9c1a29747a118e65
mat <matpataki@gmail.com>
2015-11-11 09:34:56 -0500

    seed file generator spec WIP

--- spec/models/seed_file_generator_spec.rb
+++ spec/models/seed_file_generator_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe Penman::SeedFileGenerator do
   describe '.write_seed' do
+    it "should do nothing if the table doesn't exist" do
+      ActiveRecord::Base.connection.stub(:table_exists?) { false }
+      Penman.config.after_generate.call(seed_version, 'updates')
+      Penman::RecordTag.tag(Item.first, 'updated')
+      expect {
+        Penman.generate_seeds
+      }.to_not change {
+        ActiveRecord::Base.connection.execute('select * from schema_migrations').count
+      }
+    end
 
+    it "should create the SchemaMigrations model if it doesn't already exist"
+    it "should do nothing if the schema migrations table does not incude a `versions` attribute"
+    it 'should add the new seed version to the seed_migrations table if it does exist'
   end
 end
