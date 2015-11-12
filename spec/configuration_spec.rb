6adc14c808607d7ebb7c8f237891f8344b28eced
mat <matpataki@gmail.com>
2015-11-11 20:17:40 -0500

    move specs to where they should be

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -47,5 +47,47 @@ module Penman
         expect(@config.file_name_formatter.call('Model', 'destroys')).to eq('some_crazy_Model_destroys_seed')
       end
     end
+
+    describe 'after_generate config default' do
+      before { @version = rand(99999999).to_s }
+
+      it "should do nothing if the table doesn't exist" do
+        allow(ActiveRecord::Base.connection).to receive(:table_exists?).and_return(false)
+        expect {
+          Penman.config.after_generate.call(@version, 'updates')
+        }.to_not change {
+          ActiveRecord::Base.connection.execute('select * from schema_migrations').count
+        }
+      end
+
+      it "should create the SchemaMigrations model if it doesn't already exist" do
+        Object.send(:remove_const, :SchemaMigration) if Object.const_defined?('SchemaMigration')
+        expect { SchemaMigration }.to raise_error(NameError)
+        Penman.config.after_generate.call(@version, 'updates')
+        expect { SchemaMigration }.not_to raise_error
+      end
+
+      it "should do nothing if the schema migrations table does not incude a `versions` attribute" do
+        unless Object.const_defined?('SchemaMigration')
+          Object.const_set('SchemaMigration', Class.new(ActiveRecord::Base))
+        end
+
+        allow(SchemaMigration).to receive(:column_names).and_return([])
+
+        expect {
+          Penman.config.after_generate.call(@version, 'updates')
+        }.to_not change {
+          ActiveRecord::Base.connection.execute('select * from schema_migrations').count
+        }
+      end
+
+      it 'should add the new seed version to the seed_migrations table if it does exist' do
+        expect {
+          Penman.config.after_generate.call(@version, 'updates')
+        }.to change {
+          ActiveRecord::Base.connection.execute('select * from schema_migrations').count
+        }.by(1)
+      end
+    end
   end
 end
