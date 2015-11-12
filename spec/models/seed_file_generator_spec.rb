41021f975cceb455b3cdb517c204dd28922043f6
mat <matpataki@gmail.com>
2015-11-11 21:06:16 -0500

    tighten up spec

--- spec/models/seed_file_generator_spec.rb
+++ spec/models/seed_file_generator_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe Penman::SeedFileGenerator do
   describe '.write_seed' do
     it 'should callthe `after_generate` callback' do
-      Penman.enable
-      Penman::RecordTag.tag(Item.first, 'updated')
-      Penman.disable
+      seed_file_generator = Penman::SeedFileGenerator.new(
+        'some_file_name',
+        '123456',
+        Penman::SeedCode.new
+      )
+      allow(IO).to receive(:write)
       allow(Penman.config.after_generate).to receive(:call)
-      Penman.generate_seeds
+      seed_file_generator.write_seed
       expect(Penman.config.after_generate).to have_received(:call)
     end
   end
