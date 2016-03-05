e8ff8a4a9401bfb04c4d3008206a15ac2cdcfeb0
Mat Pataki <matpataki@gmail.com>
2016-03-05 11:43:15 -0500

    setup configuration for validity check

--- spec/dummy/config/initializers/penman.rb
+++ spec/dummy/config/initializers/penman.rb
@@ -1,4 +1,5 @@
 Penman.configure do |config|
   config.seed_path = File.join(Rails.root, 'db', 'migrate')
   config.default_candidate_key = :reference
+  config.validate_records_before_seed_generation = true
 end
