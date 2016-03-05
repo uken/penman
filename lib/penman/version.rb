e797311e33fe069613ce3817507c3d0bcb41063d
Mat Pataki <matpataki@gmail.com>
2016-03-05 13:15:36 -0500

    bump version

--- lib/penman/version.rb
+++ lib/penman/version.rb
@@ -1,8 +1,7 @@
 module Penman
   MAJOR = 0     # api
-  MINOR = 3     # features
+  MINOR = 4     # features
   PATCH = 9     # bug fixes
-  BUILD = nil   # beta, rc1, etc
 
-  VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+  VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
