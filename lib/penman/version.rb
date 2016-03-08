4edf1404b20163ac3e999477bd7bfd20b5671959
devlin zed <me@devlinzed.com>
2016-03-08 15:35:41 -0500

    bump patch version

--- lib/penman/version.rb
+++ lib/penman/version.rb
@@ -1,7 +1,7 @@
 module Penman
   MAJOR = 0     # api
   MINOR = 4     # features
-  PATCH = 9     # bug fixes
+  PATCH = 10    # bug fixes
 
   VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
